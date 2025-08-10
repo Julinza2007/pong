6e691196a31de858a3275a4e12f2becfa6ba1b75
laje-E <eluneylaje@gmail.com>
2025-08-10 18:29:56 -0300

    se agrego el rebote con paletas y paredes

--- src/GUI/Ball.java
+++ src/GUI/Ball.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 public class Ball extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	int dx = 2; 
+	int dy = 2;
 
 	/**
 	 * Create the panel.
@@ -15,7 +17,7 @@ public class Ball extends JPanel {
 	public Ball(int posX, int posY, int ancho, int altura) {
 		setBounds(posX, posY, ancho, altura);
 	}
-	public void mover(int dx, int dy) {
+	public void mover() {
 		/*
 		int dx = 2; //delta x, osea la cantidad de pixeles que se va a mover la pelota, por lo tanto es la velocidad
 		int dy = 2; //delta y, velocidad
@@ -26,26 +28,41 @@ public class Ball extends JPanel {
 		posY += dy;
 		setLocation(posX, posY);
 	}
-	public void rebotar() {
+	public void rebotar(Player player1, Player player2) {
+		/*
 		int dx = 2; //delta x, osea la cantidad de pixeles que se va a mover la pelota, por lo tanto es la velocidad
 		int dy = 2; //delta y, velocidad
+		*/
 		int posY = getY();
 		int posX = getX();
-		posX += dx;
-		posY += dy;
 		if (posX <= 0 || posX >= 600 - 20) {						// 600 es el valor absoluto del ancho de la pestaña y 20 el ancho de la pelota
 																	// la primer condición es si se toca la pared izquierda, 
 																	// y la segunda es si se toca la derecha, que se hace el ancho de la cancha menos el ancho de la pelota
 																	// se hace la resta ya que la posicion de "x" se toma desde arriba a la izquierda del objeto "pelota"
-			dx *= -1;  												// se invierte el movimiento de la pelota una vez rebota en la pared derecha o izquierda
+			dx = -dx;  												// se invierte el movimiento de la pelota una vez rebota en la pared derecha o izquierda
 		}
 		
 		if (posY <= 0 || posY >= 310 - 20) {						// 310 es el valor absoluto del alto de la pestaña y 20 el alto de la pelota
 																	// la primer condición es si se toca la parte de arriba de la pantalla, 
 																	// y la segunda es si se toca la parte de abajo de la pantalla menos la altura de la pelota
 																	// se hace la resta ya que la posicion de "y" se toma desde arriba a la izquierda del objeto "pelota"
-			dy *= -1;  												// se invierte el movimiento de la pelota una vez rebota 	
+			dy = -dy;  												// se invierte el movimiento de la pelota una vez rebota 	
 		}
+		if (posX <= player1.getX() + player1.getWidth() &&
+			posX + getWidth() >= player1.getX() && 
+			posY <= player1.getY() + player1.getHeight() &&
+			posY + getHeight() >= player1.getY()) {
+			dx = -dx;
+		}
+		if (posX <= player2.getX() + player2.getWidth() &&
+			posX + getWidth() >= player2.getX() && 
+			posY <= player2.getY() + player2.getHeight() &&
+			posY + getHeight() >= player2.getY()) {
+				dx = -dx;
+			}
+		
+		posX += dx;
+		posY += dy;
 	}
 	/*
 	@Override
--- src/GUI/Pong.java
+++ src/GUI/Pong.java
@@ -19,16 +19,16 @@ public class Pong extends JFrame {
 	public Pong() {
 		
 		contentPane = new JPanel(null);
-		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
+		
 		setContentPane(contentPane);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setBounds(400, 400, 650, 349);
+		setBounds(400, 400, 616, 349);
 		setFocusable(true);
 		requestFocusInWindow();	
 		
 		Player player1 = new Player(25, 120, 10, 60);
-		Player player2 = new Player(599, 120, 10, 60);
+		Player player2 = new Player(571, 120, 10, 60);
 		
 		player1.setBackground(Color.BLACK);
 		player2.setBackground(Color.BLACK);
@@ -90,8 +90,10 @@ public class Pong extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				int cont_dx = 2;
 				int cont_dy = 2;
-				pelota.mover(cont_dx, cont_dy); //el valor inicial de la velocidad de la pelota es el valor dentro de "mover()" osea es 2 pixeles en "x" y 2 pixeles en "y".
-				pelota.rebotar();
+				int alto = contentPane.getHeight();
+				int ancho = contentPane.getWidth();
+				pelota.mover(); 					// el valor inicial de la velocidad de la pelota es el valor dentro de "mover()" osea es 2 pixeles en "x" y 2 pixeles en "y".
+				pelota.rebotar(player1, player2);	// lo mismo que el anterior
 //				pelota.repaint();
 			}
 		});
