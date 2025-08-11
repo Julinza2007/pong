dc9e6a5336c9ba3bde448c9deb68d7dfcfb44c28
julinza <linzalatajulian@gmail.com>
2025-08-11 14:05:14 -0300

    Cambiamos estructura de archivos

--- src/GUI/Pong.java
+++ src/GUI/Pong.java
@@ -3,71 +3,66 @@ package GUI;
 import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import javax.swing.Timer;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
 
 public class Pong extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+    private Puntaje puntaje;
+
 
 	public Pong() {
-		
 		contentPane = new JPanel(null);
-		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		setBounds(400, 400, 650, 349);
+		setBounds(400, 400, 616, 349);
 		setFocusable(true);
-		requestFocusInWindow();	
+		requestFocusInWindow();
 		
-		Player player1 = new Player(25, 120, 10, 60);
-		Player player2 = new Player(599, 120, 10, 60);
+		ImageIcon cancha = new ImageIcon(getClass().getResource("/GUI/canchaBuen.png"));
+		
+		Player player1 = new Player(21, 120, 10, 60);
+		Player player2 = new Player(571, 120, 10, 60);
 		
 		player1.setBackground(Color.BLACK);
 		player2.setBackground(Color.BLACK);
 		
+		puntaje = new Puntaje();
+		Ball pelota = new Ball(300, 150, 20, 20, puntaje);
+		pelota.setBackground(Color.RED);
+		contentPane.add(pelota);
+		
 		contentPane.add(player1);
 		contentPane.add(player2);
+		JLabel contenedorCancha = new JLabel(cancha);
+		contenedorCancha.setBounds(0, 0, 600, 310);
 		
-//		JPanel barrita1 = new JPanel();
-//		barrita1.setBackground(Color.BLACK);
-//		barrita1.setBounds(30, 97, 10, 66);
-//		contentPane.add(barrita1);
-		
-//		JPanel barrita2 = new JPanel();
-//		barrita2.setBackground(Color.BLACK);
-//		barrita2.setBounds(386, 73, 10, 66);
-//		contentPane.add(barrita2);			
-		
+		contentPane.add(contenedorCancha);					
 		
 		addKeyListener(new KeyAdapter() { 
-		    public void keyPressed(KeyEvent e) {
-//		    	int posXbarrita1 = barrita1.getX();
-//		    	int posYbarrita1 = barrita1.getY();
-//		    	int alturaBarrita1 = barrita1.getHeight();
-		    	
-//		    	int posYbarrita2 = barrita2.getY();
-//		    	int posXbarrita2 = barrita2.getX();
-//		    	int alturaBarrita2 = barrita1.getHeight();
-		    	
+		    public void keyPressed(KeyEvent e) {		
+		
 		    	int alturaPanel = contentPane.getHeight();
+		    	int anchoPanel = contentPane.getWidth();
+
 		    	System.out.println(alturaPanel);
+		    	System.out.println(anchoPanel);
+		    	
 
 		    	if(e.getKeyCode() == KeyEvent.VK_W) {
 		    		player1.moverArriba();
-//		    		if(posYbarrita1 > 0) {
-//		    			barrita1.setLocation(posXbarrita1, posYbarrita1 - 10);
-//		    		}
 		    	}
 		    	else if(e.getKeyCode() == KeyEvent.VK_S){
 		    		player1.moverAbajo(alturaPanel);
-//		    		if(alturaPanel > (alturaBarrita1 + posYbarrita1)) {
-//		    			barrita1.setLocation(posXbarrita1, posYbarrita1 + 10);
-//		    		}
 		    	}
 		    	
 		    	if(e.getKeyCode() == KeyEvent.VK_UP) {
@@ -78,9 +73,13 @@ public class Pong extends JFrame {
 		    	}
 		    }
 		});		
-		
-
-
+		Timer timer = new Timer(10, new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				pelota.mover(); 					// el valor inicial de la velocidad de la pelota es el valor dentro de "mover()" osea es 2 pixeles en "x" y 2 pixeles en "y".
+				pelota.rebotar(player1, player2);	// lo mismo que el anterior
+			}
+		});
+	timer.start();
 	}
 
 }
